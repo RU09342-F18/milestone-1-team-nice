a917a2b858eab2fe38d761a29d43b44eee72c635
David <sheppardd1@students.rowan.edu>
2018-10-08 15:16:49 -0400

    added switch statement for byte processing

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -1,12 +1,31 @@
-#include <msp430.h> 
+#include <msp430G2553.h>
 
 
-/**
- * main.c
- */
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	return 0;
+}
+
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void){
+
+    switch(byte){
+    case 0:
+
+        break
+    case 1:
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    default:
+
+        break;
+    }
 }
