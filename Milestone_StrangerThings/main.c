b54dbeea6ac4519abd3fa8ff509a4d767fa40c37
David <sheppardd1@students.rowan.edu>
2018-10-18 07:59:44 -0400

    Lots of commenting and removing junk

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -1,6 +1,12 @@
 #include <msp430g2553.h>
 
 //Milestone 1
+/*
+ * Written by Scott Wood and David Sheppard
+ * October 2018
+ * Controls an RGB LED using Software PWM and timers
+ * Data is set through UART which specifies the values for the colors
+ */
 
 #define UART_RXD BIT0
 /*
@@ -15,9 +21,6 @@ void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
     BCSCTL3 = LFXT1S_2;                     //interfaces with crystal (needed for clock to work)
-    //TA0CCR0 = 0xFFFF;   //set CCR0 to max possible value
-    //TA1CCR0 = 0xFFFF;   //set TA1's CCRO to max possible value
-
 
     P2DIR |= BIT1 + BIT3 + BIT5;
     P2SEL2 &= ~(BIT1 + BIT3 + BIT5);
@@ -29,31 +32,29 @@ void main(void)
     TA1CCR2 = 0x7FFF;
     TA1CCR0 = 0x7FFF;
 
-    //P1OUT = 0x00;                           // Initialize all GPIO
-    //P1DIR = 0xFF & ~UART_RXD;               // Set all pins but RXD to output
     DCOCTL = 0;                               // Select lowest DCOx and MODx settings
-    BCSCTL1 = CALBC1_8MHZ;                    // Set DCO
+    BCSCTL1 = CALBC1_8MHZ;                    // Set clock to 8 MHz
     DCOCTL = CALDCO_8MHZ;
-    UCA0BR0 = 0x41;                            // 8MHz 9600
+    UCA0BR0 = 0x41;                            // 8MHz with 9600 BAUD rate
     UCA0BR1 = 0x03;                              // 8MHz 9600
 
-    UCA0MCTL = UCBRS_3 + UCBRF_0;                        // Modulation UCBRSx = 1
+    UCA0MCTL = UCBRS_3 + UCBRF_0;             // Modulation UCBRSx = 1
     UCA0CTL1 |= UCSSEL_2;                     // SMCLK
     UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
     IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
 
     UCA0TXBUF = 0;
 
-    TA1CTL = TASSEL_2 + MC_2 + ID_0 + TAIE + TACLR; //TASSEL_2 selects SMCLK as the clock source, and MC_2 is continuous mode.
+    //TASSEL_2 selects SMCLK as the clock source, and MC_2 is continuous mode. No divider, enable interrupts, clear everything.
+    TA1CTL = TASSEL_2 + MC_2 + ID_0 + TAIE + TACLR;
+
+    //enable interrupts for CCR0, 1, and 2 of TA1
     TA1CCTL1 = CCIE;
     TA1CCTL0 = CCIE;
     TA1CCTL2 = CCIE;
 
     __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
 
-
-    //while(1);
-    //__no_operation();
 
 }
 
@@ -62,20 +63,14 @@ __interrupt void TIMER1_A(void){
 
     int x = TA1IV;
     switch(x){
-    case 2:
+    case 2: //red (CCR1) overflow
         P2OUT |= BIT1;
-        //TA1IV &= ~TA1IV_TAIFG; // Clear the Timer0 interrupt Flag
-
         break;
-    case 4:
+    case 4: //blue (CCR2) overflow
         P2OUT |= BIT5;
-        //TA1IV &= ~TA1IV_TAIFG; // Clear the Timer0 interrupt Flag
-
         break;
-    case 10:
+    case 10:    //timer overflow
         P2OUT &= ~(BIT1 + BIT3 + BIT5);
-        //TA1IV &= ~TA1IV_TAIFG; // Clear the Timer0 interrupt Flag
-
         break;
     }
 }
@@ -83,99 +78,58 @@ __interrupt void TIMER1_A(void){
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void TIMERA1_CCR0(void){
 
-    P2OUT |= BIT3;
-    //TA1IV &= ~TA1IV_TAIFG; // Clear the Timer0 interrupt Flag
-
+    P2OUT |= BIT3;  //green (CCR0) overflow
 }
 
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void){
 
-
-    /*TA1CTL = TASSEL_2 + MC_2 + ID_0; //TASSEL_2 selects SMCLK as the clock source, and MC_2 is continuous mode.
-    TA1CCTL1 = OUTMOD_7;*/
+    /*
+     * note: if 0x00 is sent through UART, the light would turn on fully because the CCR interrupt when
+        CCR = 0 cannot be generated fast enough after complete timer overflow.
+        Therefore, when the RX input is 0x00, we set the CCR to 20, which is small enough to appear off, but
+        is large enough that the processor will have time to overflow when it is reached.
+     */
 
     switch(byteNumber){
     case 0:     //calculate and send Length Byte
         numberOfBytes = UCA0RXBUF;
-        //UCA0TXBUF = (UCA0RXBUF - 3);
-        //IFG2 &= ~UCA0RXIFG; //clear interrupt flag
         break;
     case 1:     //set Red LED PWM
-        //redPWM(UCA0RXBUF);
         TA1CCR1 = ((UCA0RXBUF*256));  //0xFFFF / 257 = 255
         if(UCA0RXBUF == 0x00)
             TA1CCR1 = 30;
-        //IFG2 &= ~UCA0RXIFG; //clear interrupt flag
         break;
     case 2:     //set Green LED PWm
-        //greenPWM(UCA0RXBUF);
         TA1CCR0 = ((UCA0RXBUF*256));  //0xFFFF / 257 = 255
         if(UCA0RXBUF == 0x00)
             TA1CCR0 = 20;
-        //IFG2 &= ~UCA0RXIFG; //clear interrupt flag
         break;
     case 3:     //set Blue LED PWM
-        //bluePWM(UCA0RXBUF);
         TA1CCR2 = ((UCA0RXBUF*255));  //0xFFFF / 257 = 255
         if(UCA0RXBUF == 0x00)
             TA1CCR2 = 20;
 
+        //send off size byte now
         while (!(IFG2 & UCA0TXIFG));                // USCI_A0 TX buffer ready?
         UCA0TXBUF = (numberOfBytes - 3);
-        //IFG2 &= ~UCA0RXIFG; //clear interrupt flag
         break;
     default:    //Send the rest of the data to next node
         while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
         UCA0TXBUF = UCA0RXBUF;
-        //IFG2 &= ~UCA0RXIFG; //clear interrupt flag
         break;
     }
 
+    //increment byte number
     if(byteNumber < numberOfBytes)
         byteNumber++;
-    else
+    else    //reset byte number when all bytes have been sent
         byteNumber = 0;
 }
 
 
-/*
-void redPWM(int DC){
-    TA0CCR1 = TA0CCR0 * (DC/100);       //assuming 60% Duty cycle is sent as 60
-    TA0CTL = TASSEL_2 + MC_1 + ID_0 + TAIE; //TASSEL_2 selects SMCLK as the clock source, and MC_1 tells it to count up to the value in TA0CCR0.
-    TA0CCTL1 = OUTMOD_7;    //set-reset mode: see family user guide pg 357
-}
-
-void greenPWM(int DC){
-    TA1CCR1 = TA1CCR0 * (DC/100);       //assuming 60% Duty cycle is sent as 60
-    TA1CTL = TASSEL_2 + MC_1 + ID_0 + TAIE; //TASSEL_2 selects SMCLK as the clock source, and MC_1 tells it to count up to the value in TA0CCR0.
-    TA1CCTL1 = OUTMOD_7;    //set-reset mode: see family user guide pg 357
-}
-
-void bluePWM(int DC){
-    TA1CCR2 = TA1CCR0 * (DC/100);       //assuming 60% Duty cycle is sent as 60
-    TA1CTL = TASSEL_2 + MC_1 + ID_0 + TAIE; //TASSEL_2 selects SMCLK as the clock source, and MC_1 tells it to count up to the value in TA0CCR0.
-    TA1CCTL2 = OUTMOD_7;    //set-reset mode: see family user guide pg 357
-}*/
 
 
 
 
-/*  Example code for echoing
-
-// Echo back RXed character, confirm TX buffer is ready first
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCIAB0RX_VECTOR
-__interrupt void USCI0RX_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-  while (!(IFG2&UCAYU0TXIFG));                // USCI_A0 TX buffer ready?
-  UCA0TXBUF = UCA0RXBUF;                    // TX -> RXed character
-}
-
-*/
